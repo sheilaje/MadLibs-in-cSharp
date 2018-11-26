c010ac8b094bef3372745092e9ff05531c23468d
Scott Bergler and Sheila Stephen <scott@>
2018-11-26 14:52:43 -0800

    add second story and the controller actions, views for it.

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -6,7 +6,13 @@ namespace MadLibs.Controllers
     public class HomeController : Controller
     {
       [Route("/")]
+      public ActionResult Index() { return View(); }
+
+      [Route("/monkeyspawform")]
       public ActionResult MonkeysPawForm() { return View(); }
+
+      [Route("/maninthebrowncoatform")]
+      public ActionResult ManInTheBrownCoatForm() { return View(); }
 
       [Route("/monkeyspaw")]
       public ActionResult MonkeysPaw(string noun, string adjective, string adverb, string verb) {
@@ -17,6 +23,16 @@ namespace MadLibs.Controllers
         newObject.SetVerb(verb);
         return View(newObject);
         }
+
+        [Route("/maninthebrowncoat")]
+        public ActionResult ManInTheBrownCoat(string noun, string adjective, string adverb, string verb) {
+          PartsOfSpeech newObject = new PartsOfSpeech();
+          newObject.SetNoun(noun);
+          newObject.SetAdjective(adjective);
+          newObject.SetAdverb(adverb);
+          newObject.SetVerb(verb);
+          return View(newObject);
+          }
 
     }
 }
